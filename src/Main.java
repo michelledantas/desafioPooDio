e681c4ab92ccd41f122a4b3c1d73a1b80eac8d72
Michelle <michelledantasluiz@gmail.com>
2021-08-20 16:19:07 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -16,13 +15,45 @@ public class Main {
         curso2.setDescricao("descricao curso js");
         curso2.setCargaHoraria(4);
 
+        Conteudo conteudo = new Curso();
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMichelle = new Dev();
+        devMichelle.setNome("Michelle");
+        devMichelle.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Michelle" + devMichelle.getConteudosInscritos());
+        devMichelle.progredir();
+        System.out.println("Conteúdos Inscritos Michelle" + devMichelle.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Michelle" + devMichelle.getConteudosConcluidos());
+        System.out.println("XP " + devMichelle.calcularTotalXp());
+
+        System.out.println("----------");
+
+        Dev devMichel = new Dev();
+        devMichel.setNome("Michel");
+        devMichel.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devMichel.getConteudosInscritos());
+        devMichel.progredir();
+        System.out.println("Conteúdos Inscritos" + devMichel.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Michel" + devMichel.getConteudosConcluidos());
+        System.out.println("XP " + devMichel.calcularTotalXp());
+
+
+
     }
+
 }
--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -7,7 +7,23 @@ public abstract class Conteudo {
     private String titulo;
     private String descricao;
 
-    //teste
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
     public abstract double calcularXp();
+
+
 }
